69df32cb7b48139630efdfd9f879c2a01628779d
Mary Jack <maryjackinthebox@gmail.com>
2018-04-24 22:37:40 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,7 +2,16 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailParser 
+  
+  attr_accessor :email
+  
+  @@all = []
 
+  def self.all
+    @@all
+  end
+  
 def self.new_from_filename(filename)
     song_array = filename.split(" - ")
     song = self.new
@@ -10,3 +19,5 @@ def self.new_from_filename(filename)
     song.artist_name = song_array[0]
     song
   end
+
+end
