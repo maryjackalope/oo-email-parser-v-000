bfa7f22f0a54c9a0b5ee0af7b653a339eeaac8b8
Mary Jack <maryjackinthebox@gmail.com>
2018-04-24 22:38:31 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,20 +4,8 @@
 # or whitespace (' ').
 class EmailParser 
   
-  attr_accessor :email
+  attr_accessor :emails
   
-  @@all = []
-
-  def self.all
-    @@all
-  end
-  
-def self.new_from_filename(filename)
-    song_array = filename.split(" - ")
-    song = self.new
-    song.name = song_array[1].split(".")[0]
-    song.artist_name = song_array[0]
-    song
-  end
+ def initialize (emails)
 
 end
